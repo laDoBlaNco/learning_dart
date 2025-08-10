51a1279222c482cb7b86f7a41cca942f3694f520
LaDoBlaNco <whitesidekevin@gmail.com>
2025-08-09 21:46:51 -0400

    Starting youtube course mahdi Mirzdeh Dart Programming as of 8/8/25

---
+++ courses/youtube/mahdiMirzdeh/dartProgramming/introMath.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('Hello, Dart!');
+  print(2 + 2);
+  print(4 - 2);
+  print(4 * 2);
+  print(4 / 2);
+  print(4 + 4 / 2);
+  print((4 + 4) / 2);
+  print((4 + 4) / 3);
+  print((4 + 4) ~/ 3); // integer division operator (truncates)
+  print(4 % 2);
+}
